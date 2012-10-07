196d249b7a2aeed96f1cba6859bad9561c47c0db
tombuildsstuff <github@ibuildstuff.co.uk>
2012-10-07 17:34:21 +0100

    Updated the version to 1.3 Shared the version number across each child project through the use of a common assembly info file

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Tom Harvey")]
+[assembly: AssemblyProduct("Simple Backup")]
+[assembly: AssemblyCopyright("Copyright © Tom Harvey 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
